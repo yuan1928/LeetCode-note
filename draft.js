27ab2edd1c5f92fe92dae00af354591c0d85b2bf
GXY <1261461549@qq.com>
2022-01-26 02:22:30 +0800

    2022/1/26

--- draft.js
+++ draft.js
@@ -1,19 +1,57 @@
-var numberOfMatches = function(n) {
-    let res=0
-    while (n>1)
+var DetectSquares = function() {
+    this.points = {}
+};
+
+DetectSquares.prototype.add = function(point) {
+    const name = point[0]+"/"+point[1]
+    if(name in this.points){this.points[name].count+=1}
+    else {this.points[name] = {count:1,value:point}}
+};
+
+DetectSquares.prototype.count = function(point) {
+    const X = []
+    const Y = []
+    let cur
+    for(let p in this.points)
     {
-        if(n%2===0)
+        cur=this.points[p]
+        if(cur.value[0]===point[0] && cur.value[1]!==point[1]) {Y.push(cur)}
+        else if(cur.value[0]!==point[0] && cur.value[1]===point[1]) {X.push(cur)}
+    }
+
+    let res=0
+    let pX
+    let pY
+    for(let x of X)
+    {
+        for(let y of Y)
         {
-            res+=(n/2)
-            n=n/2
-        }
-        else
-        {
-            res+=((n-1)/2)
-            n=1+((n-1)/2)
+            if(Math.abs(x.value[0]-point[0])===Math.abs(y.value[1]-point[1]))
+            {
+                pX=x.value[0]-point[0]+y.value[0]
+                pY=y.value[1]-point[1]+x.value[1]
+                if(pX+"/"+pY in this.points)
+                {res+=(x.count*y.count*this.points[pX+"/"+pY].count)}
+            }
         }
     }
     return res
 };
-console.log(numberOfMatches(7));
-console.log(numberOfMatches(14));
+
+const add=function (dest,src)
+{
+    for(let p of src)
+    {dest.add(p)}
+}
+
+const test=new DetectSquares()
+/*for(let p of [[3,10],[11,2],[3,2]]) {test.add(p)}
+console.log(test.count([11, 10]));
+console.log(test.count([14, 8]));
+test.add([11,2])
+console.log(test.count([11, 10]));*/
+const add1=[[5,10],[10,5],[10,10]]
+const add2=[[3,0],[8,0],[8,5]]
+const add3=[[9,0],[9,8],[1,8]]
+add(test,[...add1,...add2,...add3,[0,0],[8,0],[8,8]])
+console.log(test.count([0,8]));
