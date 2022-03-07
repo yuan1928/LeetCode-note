761c6970240d877fca57c26d24d09d4a6340a8ef
GXY <1261461549@qq.com>
2022-03-07 11:55:50 +0800

    2022/3/7

--- 10.数据结构/1.数组/1005_k次取反后最大的数组和.js
+++ 10.数据结构/1.数组/1005_k次取反后最大的数组和.js
@@ -1,7 +1,7 @@
 //min+rest=sum->rest=sum-min->-min+rest=sum-2*min
 //贪心原则：每次改变数组中最小值
 var largestSumAfterKNegations = function(nums, k) {
-    if(nums.length===1)return -nums[0]
+    /*if(nums.length===1)return -nums[0]
 
     nums.sort((a,b)=>(a-b))
     let sum=nums.reduce((a,b)=>(a+b))
@@ -23,8 +23,43 @@ var largestSumAfterKNegations = function(nums, k) {
             }
         }
     }
-    return sum
+    return sum*/
+
+    //(1)全部非负：
+    //1)k为偶->res=sum(nums)
+    //2)k为奇->res=sum(nums)-2*min
+    //(2)存在负数：
+    //1)k=count(负数)->res=sum(nums负数全部取反)
+    //2)k<count(负数)->res=sum(nums)+2*sum(最小K个负数分别取反)
+    //3)k>count(负数)->nums=nums负数全部取反,k=k-count(负数)->(1)
+    const allPositives=function (K,NUMS){
+        if(K%2===0)return NUMS.reduce((a,b)=>(a+b))
+        else {return NUMS.reduce((a,b)=>(a+b))-2*Math.min(...NUMS)}
+    }
+
+    if(Math.min(...nums)>=0) {return allPositives(k,nums)}
+    else
+    {
+        let negative=[]
+        let negativeCount=0
+        nums.forEach((v)=>{
+            if(v<0)
+            {
+                negative.push(v)
+                negativeCount+=1
+            }
+        })
+
+        if(k===negativeCount)return nums.reduce((a,b)=>(Math.abs(a)+Math.abs(b)))
+        else if(k>negativeCount){return allPositives(k-negativeCount,nums.map((v)=>(v<0?-v:v)))}
+        else
+        {
+            negative.sort((a,b)=>(a-b))
+            return nums.reduce((a,b)=>(a+b))+2*(k>1?negative.slice(0,k).reduce((a,b)=>(Math.abs(a)+Math.abs(b))):-negative[0])
+        }
+    }
 };
-console.log(largestSumAfterKNegations([4, 2, 3], 1));
-console.log(largestSumAfterKNegations([3, -1, 0, 2], 3));
-console.log(largestSumAfterKNegations(nums = [2, -3, -1, 5, -4], k = 2));
+//console.log(largestSumAfterKNegations([4, 2, 3], 1));
+//console.log(largestSumAfterKNegations([3, -1, 0, 2], 3));
+//console.log(largestSumAfterKNegations(nums = [2, -3, -1, 5, -4], k = 2));
+console.log(largestSumAfterKNegations([9, -4, -7, 9, 6], 1));
--- 10.数据结构/1.数组/769_最多能完成排序的块.js
+++ 10.数据结构/1.数组/769_最多能完成排序的块.js
@@ -1,5 +1,5 @@
 var maxChunksToSorted = function(arr) {
-    if(arr.length===1)return 1
+    /*if(arr.length===1)return 1
 
     let res=1
     let pos=arr.length-1
@@ -10,6 +10,27 @@ var maxChunksToSorted = function(arr) {
         if(min>Math.max(...arr.slice(0,pos))){res+=1}//当前块的最小值大于其左块的最大值->产生一个能完成排序的独立子块
         pos-=1
     }
-    return res
+    return res*/
+
+    const max=[arr[0]]
+    const min=[arr[arr.length-1]]
+    let j
+    for(let i=1; i<=arr.length-1; i++)
+    {
+        max.push(Math.max(max[i-1],arr[i]))
+        j=arr.length-1-i
+        min.unshift(Math.min(min[min.length-1-(i-1)],arr[j]))
+    }
+    //console.log(min,max);
+
+    let res=0
+    for(let i=0; i<arr.length-1; i++)
+    {
+        if(max[i]<=min[i+1])
+        {res+=1}
+    }
+    return res+1
 };
-console.log(maxChunksToSorted([0,1]));
+//console.log(maxChunksToSorted([0,1]));
+console.log(maxChunksToSorted([1, 0, 2, 3, 4]));
+console.log(maxChunksToSorted([4, 3, 2, 1, 0]));
